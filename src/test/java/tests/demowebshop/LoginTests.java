74d8212766dea133cf477d564c8ce2da46190495
Maksim Skomorokhov <nullnvrsk@gmail.com>
2025-01-04 19:40:45 +0300

    Added LoginTests

---
+++ src/test/java/tests/demowebshop/LoginTests.java
@@ -0,0 +1,71 @@
+package tests.demowebshop;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+
+public class LoginTests {
+
+    String login = "mxlamer@mail.ru";
+    String password = "123456";
+
+    @BeforeAll
+    public static void setup() {
+        Configuration.baseUrl = "https://demowebshop.tricentis.com";
+        RestAssured.baseURI = "https://demowebshop.tricentis.com";
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @Test
+    void loginWithUITest() {
+        step("Open login page", () ->
+                open("/login"));
+
+        step("Fill login form", () -> {
+            $("#Email").setValue(login);
+            $("#Password").setValue(password).pressEnter();
+        });
+
+        step("Verify successful authentication", () ->
+                $(".account").shouldHave(text(login)));
+    }
+
+    @Test
+    void loginWithApiTest() {
+        step("Get auth cookie by API and set it to browser ", () -> {
+            String authCookieKey = "NOPCOMMERCE.AUTH";
+            String authCookieValue = given()
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+                .when()
+                    .post("/login")
+                .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract()
+                    .cookie(authCookieKey);
+
+            open("/favicon.ico");
+            Cookie authCookie = new Cookie(authCookieKey, authCookieValue);
+            getWebDriver().manage().addCookie(authCookie);
+        });
+
+        step("Open main page", () ->
+                open("/"));
+
+        step("Verify successful authentication", () ->
+                $(".account").shouldHave(text(login)));
+    }
+}
